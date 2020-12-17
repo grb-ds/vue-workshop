ffb8d321e4402eb6c3dab3fbe2ae8bb842415b48
Giomayra Rosales <giomayra.rosales@gmail.com>
2020-12-17 11:32:24 +0100

    Add Image Slider

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,85 @@
 <template>
+  <div class="wrapper">
+    <!-- Header -->
+    <h1 class="header">{{ amount }} Images 📸</h1>
+
+    <!-- Slider -->
+    <input class="slider" type="range" min="3" max="60" v-model="amount" />
+
+    <!-- Images -->
+    <div class="image__wrapper" v-if="images">
+      <a
+          class="image__link"
+          v-for="image in images"
+          :key="image.id"
+          :href="image.url"
+      >
+        <img :src="image.download_url" class="image" />
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    // This method is executed when page loads
+    this.getImages(this.amount);
+  },
+  data() {
+    return {
+      amount: 10,
+      images: null,
+    };
+  },
+  methods: {
+    async getImages(amount) {
+      // This method fetches the images
+      const response = await fetch(
+          `https://picsum.photos/v2/list?limit=${amount}`
+      );
+      const images = await response.json();
+      this.images = images;
+    },
+  },
+  watch: {
+    amount() {
+      // Everytime the amount changes this code will be executed
+      this.getImages(this.amount);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.wrapper {
+  @apply relative container h-full mx-auto px-3 space-y-5;
+}
+
+.header {
+  @apply mb-4 text-3xl;
+}
+
+.slider {
+  @apply w-10/12 sm:w-3/6 md:w-72;
+}
+
+.image__wrapper {
+  @apply grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 items-stretch justify-items-stretch;
+}
+.image__link {
+  @apply w-full;
+}
+.image {
+  @apply object-cover h-52;
+  @apply w-full shadow-md rounded-2xl;
+  @apply border-2 border-transparent;
+  @apply hover:border-gray-800 hover:shadow-xl;
+}
+</style>
+
+<!--
+<template>
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
@@ -42,7 +123,7 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+&lt;!&ndash; Add "scoped" attribute to limit CSS to this component only &ndash;&gt;
 <style scoped>
 h3 {
   margin: 40px 0 0;
@@ -59,3 +140,4 @@ a {
   color: #42b983;
 }
 </style>
+-->
